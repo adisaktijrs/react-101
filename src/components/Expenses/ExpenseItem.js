2073a0cd724ae4c560b98f93094c48883c069376
adisaktijrs <adhisakti47@gmail.com>
2022-05-07 22:52:20 +0700

    Finish chapter 5

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,27 +1,20 @@
-import React, { useState } from 'react';
 
 import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card';
 import './ExpenseItem.css';
 
 const ExpenseItem = (props) => {
-  const [title, setTitle] = useState(props.title);
-
-  const clickHanlder = () => {
-    setTitle('lol');
-    console.log('Clicked!');
-  };
-
-  return (
-    <Card className='expense-item'>
-      <ExpenseDate date={props.date} />
-      <div className='expense-item__description'>
-        <h2>{title}</h2>
-        <div className='expense-item__price'>${props.amount}</div>
-      </div>
-      <button onClick={clickHanlder}>Change Title</button>
-    </Card>
-  );
+	return (
+		<li>
+			<Card className='expense-item'>
+				<ExpenseDate date={props.date} />
+				<div className='expense-item__description'>
+					<h2>{props.title}</h2>
+					<div className='expense-item__price'>${props.amount}</div>
+				</div>
+			</Card>
+		</li>
+	);
 }
 
 export default ExpenseItem;
